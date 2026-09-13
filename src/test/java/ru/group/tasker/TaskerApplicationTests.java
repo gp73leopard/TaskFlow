31c7ffee0d2bd4ef8e971a19fe1e5eb15c57ed72
gp73l <gp73leopard@gmail.com>
2026-09-13 22:45:45 +0400

    init project

---
+++ src/test/java/ru/group/tasker/TaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.group.tasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
